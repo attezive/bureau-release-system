6ec3d38b3d55d64e82d1c528a65e17519c2c97f2
Attezive <attezive@gmail.com>
2025-07-11 10:44:15 +0300

    Added Java Project

---
+++ src/main/java/org/example/bureaureleasesystem/BureauReleaseSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.bureaureleasesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BureauReleaseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BureauReleaseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bureaureleasesystem/BureauReleaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bureaureleasesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BureauReleaseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
